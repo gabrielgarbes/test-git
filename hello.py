a6b2a51040f7657a64387a8acc8f704f4f91f822
Gabriel Garbes <gabriel.garbes@mac.com.br>
2023-06-07 09:54:31 -0300

    extracted cli argument to variable

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
 import sys
 
-if str(sys.argv[1]) != None:
+name = None
+try:
+    name = str(sys.argv[1])
+except:
+    name = ''
+
+if name != '':
     print("Hello {}!".format(sys.argv[1]))
 else:
     print('Hello')
